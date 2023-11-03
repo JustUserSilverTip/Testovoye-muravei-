5874cd76ef0af9fa183624abadd754a4716450e7
MrFortuna <44096066+JustUserSilverTip@users.noreply.github.com>
2023-11-03 20:55:01 +0700

    Delete func directory

--- func/create_blank_image.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-
-def create_blank_image(width, height):
-    return Image.new("1", (width, height), color=1)
--- func/update_direction_and_pixel.py
+++
@@ -1,8 +0,0 @@
-def update_direction_and_pixel(x, y, direction, pixels):
-    if pixels[y][x] == 1:  # Если клетка белая
-        direction = (direction + 1) % 4  # Поворачиваем на 90° по часовой стрелке
-        pixels[y][x] = 0  # Инвертируем пиксель (черный)
-    else:  # Если клетка черная
-        direction = (direction - 1) % 4  # Поворачиваем на 90° против часовой стрелки
-        pixels[y][x] = 1  # Инвертируем пиксель (белый)
-    return direction
